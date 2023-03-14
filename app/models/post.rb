3c45fc17758320175a642c1d72feb03dbb5def76
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-03-14 19:48:46 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    # In Built methods
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: ["Fiction","Non-Fiction"]}
+
+    #Custom methods
+    validate :title_is_sufficiently_clickbait_y?
+
+    def title_is_sufficiently_clickbait_y?
+		valids = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+		if valids.none?{|par| par.match title}
+			errors.add(:title, "not clickbait")
+		end
+	end
 end
